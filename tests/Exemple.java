2495bc1e6d00ae90b8aaaa80bec7077836357cca
bapateyron <bastien.pateyron@etu.isima.fr>
2020-01-07 15:09:01 +0100

    First Commit

---
+++ tests/Exemple.java
@@ -0,0 +1,8 @@
+public class Exemple
+{
+   public static void main(String[] argv)
+   {
+      // java.lang.System.out.println("Hello world"); // Le java.lang est le package qui contiens la classe System
+      System.out.println("Hello world");
+   }
+}
