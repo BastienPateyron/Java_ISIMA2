bd8402853ac3bd76e4f917a0120b6c18078b2434
bapateyron <bastien.pateyron@etu.isima.fr>
2020-01-07 16:32:24 +0100

    1.1.1

--- 1/Exemple.java
+++ 1/Exemple.java
@@ -1,8 +1,57 @@
-public class Exemple
-{
-   public static void main(String[] argv)
+// import java.util.StringTokenizer;
+
+public class Exemple {
+   public static void main(String[] argv) {
+      exo1();
+      test1();
+      exo2(argv);
+   }
+
+   public static void exo1()
    {
-      // java.lang.System.out.println("Hello world"); // Le java.lang est le package qui contiens la classe System
-      System.out.println("Hello world");
+      // Quelques déclarations préalables
+      String s1 = "loic";
+      String s2 = "loic";
+      String s3 = new String("loic");
+      String s4 = new String("loic");
+      String s5 = s3;
+      String s6 = null;
+
+      // Test de l'opérateur égal ==
+      System.out.print("s1==s2? ");
+      System.out.println(s1 == s2);
+      System.out.print("s1==s3? ");
+      System.out.println(s1 == s3);
+      System.out.print("s3==s4? ");
+      System.out.println(s3 == s4);
+      System.out.print("s5==s3? ");
+      System.out.println(s5 == s3);
+
+      // Test de la méthode equals()
+      System.out.print("s1.equals(s3)? ");
+      System.out.println(s1.equals(s3));
+      System.out.print("s1.equals(s6)? ");
+      System.out.println(s1.equals(s6));
+      // System.out.print("s6.equals(s1)? ");
+      // System.out.println(s6.equals(s1));
+   }
+
+   public static void exo2(String[] argv)
+   {
+      for(String item : argv)
+      {
+         System.out.println(item);
+      }
+   }
+
+   public static void test1()
+   {
+      // Dans une regex: '\s' correspond à un espace
+      // Comme le '\' est interprété dans une string, on doit l'échapper
+      String[] resultat = "je suis une très grande chaine !".split("\\s");
+      for(String item : resultat)
+      {
+         System.out.println(item);
+      }
    }
 }
